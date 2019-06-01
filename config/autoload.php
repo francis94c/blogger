eb498b6abce2c08aac7bbf595e1c9526682de23b
Francis Ilechukwu <francis94c@gmail.com>
2019-06-01 21:34:47 +0100

    Autoloaded centralized application code base.

---
+++ config/autoload.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Libraries
+ * @var [type]
+ */
+$autoload["libraries"] = array(array("BloggerAppCore", "core"));
+?>
