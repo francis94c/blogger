b437f3223d19c42485b68a8bdc2298d9a809608d
Francis Ilechukwu <francis94c@gmail.com>
2019-06-01 10:30:54 +0100

    Empty view optimization on loading scripts.

--- views/empty_view.php
+++ views/empty_view.php
@@ -1,5 +1,8 @@
-<?=w3css();?>
-<?=fontsawesome();?>
+<?php
+$ci =& get_instance();
+?>
+<?=$ci->config->item("francis94c/blogger/w3css") == true ? w3css() : "";?>
+<?=$ci->config->item("francis94c/blogger/fontawesome") == true ? fontsawesome() : "";?>
 <div class="w3-center">
   <p><i class="fa fa-sun fa-4x"></i></p>
   <h1>No posts to display.</h1>
