bb86f593f083582abfedd07ef2124f3212cf30d4
Francis Ilechukwu <francis94c@gmail.com>
2019-06-02 00:28:04 +0100

    Added posts list to admin menu.

--- views/admin.php
+++ views/admin.php
@@ -7,4 +7,12 @@
       </div>
     </a>
   </div>
+  <div class="w3-quarter w3-padding">
+    <a href="<?=isset($new_post_url) ? $new_post_url : app_url("admin/list_posts")?>" class="link">
+      <div class="w3-card w3-round w3-teal w3-padding">
+        <p class="w3-center"><i class="fas fa-list-alt fa-4x"></i></p>
+        <p class="w3-center w3-large">Posts</p>
+      </div>
+    </a>
+  </div>
 </div>
