763663a42af6a1b8dcf89d505f7401f04d1b363a
Francis Ilechukwu <francis94c@gmail.com>
2019-06-01 21:33:57 +0100

    Admin menu view.

---
+++ views/admin.php
@@ -0,0 +1,10 @@
+<div class="w3-row">
+  <div class="w3-quarter w3-padding">
+    <a href="<?=isset($new_post_url) ? $new_post_url : app_url("admin/new_post")?>" class="link">
+      <div class="w3-card w3-round w3-blue w3-padding">
+        <p class="w3-center"><i class="fas fa-plus-square fa-4x"></i></p>
+        <p class="w3-center w3-large">New Post</p>
+      </div>
+    </a>
+  </div>
+</div>
